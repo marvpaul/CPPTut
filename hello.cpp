6d4c9e62e641a6bd730730854b8e2919ef008ca5
marvpaul <marvin.krueger@yahoo.de>
2018-08-30 11:00:49 +0200

    Initial commit with hello world

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+//Including library for using consol output
+#include <iostream>
+
+//Defining a namespace for our program
+using namespace std; 
+
+int main(int argc, char** argv){
+    cout << "Hello World"; 
+
+    //Return zero means our program has executed sucessfully
+    return 0; 
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Introduction to c++
+Referenz
+https://www.luis.uni-hannover.de/cplus.html
+
+## Lesson 1
+Einführung in Arbeit mit Git
+
+Grundlegender Aufbau Main Programm (61. - 78. 1. VL)
+
+Kompilieren (94-96 1. VL)
